40e0f33a99ea9fbcf1c99918abaa329b7c2ea3fe
Richard Rutsche <richardrutsche@gmail.com>
2016-03-05 20:01:49 +0100

    change bgimage and children dynamically in kitchensink

--- src/KitchenSink.jsx
+++ src/KitchenSink.jsx
@@ -1,13 +1,36 @@
 import React from 'react';
-import { Parallax, Background } from '../dist/index';
+import { Parallax, Background } from './jsx/index.jsx';
 
 export default class KitchenSink extends React.Component {
 
 	constructor(props) {
 		super(props);
-
-		this.state = {
+		let fontStyle = {
+			fontFamily: 'Helvetica Neue, Arial, sans-serif',
+			textAlign: 'center',
+			fontWeight: 100
 		};
+		this.state = {
+			image: require('./assets/1.jpg'),
+			children: <h1 style={fontStyle}>children no1</h1>
+		};
+	}
+
+	changeImage() {
+		this.setState({
+			image: require('./assets/2.jpg')
+		});
+	}
+
+	changeChildren() {
+		let fontStyle = {
+			fontFamily: 'Helvetica Neue, Arial, sans-serif',
+			textAlign: 'center',
+			fontWeight: 100
+		};
+		this.setState({
+			children: <h1 style={fontStyle}>children no2!!!!</h1>
+		});
 	}
 
 	render() {
@@ -15,11 +38,6 @@ export default class KitchenSink extends React.Component {
 			backgroundColor: '#efefef',
 			color: 'white',
 			textAlign: 'center'
-		};
-		let fontStyle = {
-			fontFamily: 'Helvetica Neue, Arial, sans-serif',
-			textAlign: 'center',
-			fontWeight: 100
 		};
 		let fontStyle2 = {
 			fontFamily: 'Helvetica Neue, Arial, sans-serif',
@@ -35,10 +53,12 @@ export default class KitchenSink extends React.Component {
 					<h1 style={fontStyle2}>One morning, when Gregor Samsa woke from troubled dreams, he found himself transformed in his bed into a horrible vermin. He lay on his armour-like back, and if he lifted his head a little he could see his brown belly, slightly domed and divided by arches into stiff sections.</h1>
 					<h1 style={fontStyle2}>One morning, when Gregor Samsa woke from troubled dreams, he found himself transformed in his bed into a horrible vermin. He lay on his armour-like back, and if he lifted his head a little he could see his brown belly, slightly domed and divided by arches into stiff sections.</h1>
 					<br/>
+					<h1 style={fontStyle2} onClick={() => this.changeImage()}>change image</h1>
+					<h1 style={fontStyle2} onClick={() => this.changeChildren()}>change children</h1>
 				</div>
-				<Parallax bgImage={require('./assets/1.jpg')} strength={400} log={true}>
+				<Parallax bgImage={this.state.image} strength={400} log={true}>
 					<br/>
-					<h1 style={fontStyle}>...</h1>
+					{this.state.children}
 					<h1 style={fontStyle2}>A collection of textile samples lay spread out on the table - Samsa was a travelling salesman - and above it there hung a picture that he had recently cut out of an illustrated magazine and housed in a nice, gilded frame. It showed a lady fitted out with a fur hat and fur boa who sat upright, raising a heavy fur muff that covered the whole of her lower arm towards the viewer. Gregor then turned to look out the window at the dull weather. Drops</h1>
 					<br/>
 				</Parallax>
@@ -47,14 +67,14 @@ export default class KitchenSink extends React.Component {
 					<h1 style={fontStyle2}>A collection of textile samples lay spread out on the table - Samsa was a travelling salesman - and above it there hung a picture that he had recently cut out of an illustrated magazine and housed in a nice, gilded frame. It showed a lady fitted out with a fur hat and fur boa who sat upright, raising a heavy fur muff that covered the whole of her lower arm towards the viewer. Gregor then turned to look out the window at the dull weather. Drops</h1>
 					<br/>
 				</div>
-				<Parallax strength={300}>
+				{/*<Parallax strength={300}>
 					<Background>
 						<img src="http://www.fillmurray.com/400/300" alt="fill murray"/>
 						<div style={{width: 800, height: 300, backgroundColor: '#450093'}}></div>
 						<img src="http://www.fillmurray.com/500/300" alt="fill murray"/>
 					</Background>
 					<h1 style={fontStyle2}>A collection of textile samples lay spread out on the table - Samsa was a travelling salesman - and above it there hung a picture that he had recently cut out of an illustrated magazine and housed in a nice, gilded frame. It showed a lady fitted out with a fur hat and fur boa who sat upright, raising a heavy fur muff that covered the whole of her lower arm towards the viewer. Gregor then turned to look out the window at the dull weather. Drops</h1>
-				</Parallax>
+				</Parallax>*/}
 
 				<div>
 					<br/>
