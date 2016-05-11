99c1592242a8b06ccfccbd0d66f7b4437734162d
Richard Rutsche <richardrutsche@gmail.com>
2016-05-11 12:21:03 +0200

    new getPosition function returns relative value depending on screen position and content height

--- src/util/Util.js
+++ src/util/Util.js
@@ -27,22 +27,25 @@ export function canUseDOM() {
 	return !!((typeof window !== 'undefined' && window.document && window.document.createElement));
 }
 
-// export function getPosition(element, canUseDOM) {
-// 	if (!canUseDOM) {
-// 		return 0;
-// 	}
-
-// 	let elementTop = element.getBoundingClientRect().top;
-
-// 	console.log(elementTop, element.offsetTop);
-// }
-
-export function getPosition(element) {
-    var yPosition = 0;
+export function getPosition(node, canUseDOM) {
+	if (!canUseDOM) {
+		return 0;
+	}
+	let element = node;
+	let height = node.getBoundingClientRect().height;
+    let y = 0;
   
     while(element) {
-        yPosition += (element.offsetTop - element.scrollTop + element.clientTop);
+        y += (element.offsetTop - element.scrollTop + element.clientTop);
         element = element.offsetParent;
     }
-    return { y: yPosition };
+
+    y = y > window.innerHeight ? window.innerHeight : (y < (-height) ? 0 : y);
+    // this is a value between 0 - 1 where 0 is top and 1 is bottom of the screen
+    let relativePosition = y / window.innerHeight;
+    // the relative anchor represents the relative position on screen including the DOMNode heigth
+    // so value does not get 0 before bottom of element reaches top of the screen
+    let relativeAnchor = relativePosition + ((1 - relativePosition) * height / window.innerHeight);
+
+    return relativeAnchor;
 }
