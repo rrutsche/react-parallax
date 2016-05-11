50de310b2b8937c27de38cd0276beff43e7ff24b
Richard Rutsche <richardrutsche@gmail.com>
2016-05-11 12:20:08 +0200

    more text for kitchensink

--- src/KitchenSink.jsx
+++ src/KitchenSink.jsx
@@ -56,6 +56,15 @@ export default class KitchenSink extends React.Component {
 					<h1 style={fontStyle2} onClick={() => this.changeImage()}>change image</h1>
 					<h1 style={fontStyle2} onClick={() => this.changeChildren()}>change children</h1>
 				</div>
+				<div>
+					<br/>
+					<h1 style={fontStyle2}>One morning, when Gregor Samsa woke from troubled dreams, he found himself transformed in his bed into a horrible vermin. He lay on his armour-like back, and if he lifted his head a little he could see his brown belly, slightly domed and divided by arches into stiff sections.</h1>
+					<h1 style={fontStyle2}>One morning, when Gregor Samsa woke from troubled dreams, he found himself transformed in his bed into a horrible vermin. He lay on his armour-like back, and if he lifted his head a little he could see his brown belly, slightly domed and divided by arches into stiff sections.</h1>
+					<h1 style={fontStyle2}>One morning, when Gregor Samsa woke from troubled dreams, he found himself transformed in his bed into a horrible vermin. He lay on his armour-like back, and if he lifted his head a little he could see his brown belly, slightly domed and divided by arches into stiff sections.</h1>
+					<br/>
+					<h1 style={fontStyle2} onClick={() => this.changeImage()}>change image</h1>
+					<h1 style={fontStyle2} onClick={() => this.changeChildren()}>change children</h1>
+				</div>
 				<Parallax bgImage={this.state.image} strength={400} log={true}>
 					<br/>
 					{this.state.children}
@@ -78,6 +87,9 @@ export default class KitchenSink extends React.Component {
 
 				<div>
 					<br/>
+					<h1 style={fontStyle2}>A collection of textile samples lay spread out on the table - Samsa was a travelling salesman - and above it there hung a picture that he had recently cut out of an illustrated magazine and housed in a nice, gilded frame. It showed a lady fitted out with a fur hat and fur boa who sat upright, raising a heavy fur muff that covered the whole of her lower arm towards the viewer. Gregor then turned to look out the window at the dull weather. Drops</h1>
+					<h1 style={fontStyle2}>A collection of textile samples lay spread out on the table - Samsa was a travelling salesman - and above it there hung a picture that he had recently cut out of an illustrated magazine and housed in a nice, gilded frame. It showed a lady fitted out with a fur hat and fur boa who sat upright, raising a heavy fur muff that covered the whole of her lower arm towards the viewer. Gregor then turned to look out the window at the dull weather. Drops</h1>
+					<h1 style={fontStyle2}>A collection of textile samples lay spread out on the table - Samsa was a travelling salesman - and above it there hung a picture that he had recently cut out of an illustrated magazine and housed in a nice, gilded frame. It showed a lady fitted out with a fur hat and fur boa who sat upright, raising a heavy fur muff that covered the whole of her lower arm towards the viewer. Gregor then turned to look out the window at the dull weather. Drops</h1>
 					<h1 style={fontStyle2}>A collection of textile samples lay spread out on the table - Samsa was a travelling salesman - and above it there hung a picture that he had recently cut out of an illustrated magazine and housed in a nice, gilded frame. It showed a lady fitted out with a fur hat and fur boa who sat upright, raising a heavy fur muff that covered the whole of her lower arm towards the viewer. Gregor then turned to look out the window at the dull weather. Drops</h1>
 					<br/>
 				</div>
