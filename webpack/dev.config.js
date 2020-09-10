7fb8521d8bb404ff4a2b32ca37ffdb314b4e98c7
Richard Rutsche <richardrutsche@posteo.de>
2020-09-10 11:47:52 +0200

    PAR-27: bump packages to actual versions

--- webpack/dev.config.js
+++ webpack/dev.config.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const merge = require('webpack-merge');
+const { merge } = require('webpack-merge');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const common = require('./base.config.js');
